8d6d1d21b0b63b4cbf0542498152e734dc919a02
RezaRahimi166 <rezaa.salami99@gmail.com>
2024-11-20 12:28:10 -0900

    feat(courseDetails): creating section

--- src/features/coursers/components/Course.jsx
+++ src/features/coursers/components/Course.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Course = ({
   title,
@@ -7,13 +8,16 @@ const Course = ({
   description,
   duration,
   numOfReviews,
+  id,
 }) => {
   return (
     <div className="card">
       <img src={coverImageUrl} alt={title} className="card-img-top" />
       <div className="card-header px-4 pt-4 pb-0">
         <div className="badge bg-primary my-2 fw-bolder">{courseLevel}</div>
-        <h4 className="mb-0">{title}</h4>
+        <h4 className="mb-0">
+          <Link to={`/courses/${id}`}>{title}</Link>
+        </h4>
       </div>
       <div className="card-body px-4 pt-2">
         <p className="text-truncate-3">{description}</p>
